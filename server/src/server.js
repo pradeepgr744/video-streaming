df7fcb39ff9f8118f14626fd5bd9b6a9cb0983a2
pradeepgr744 <dseeeap2020@gmail.com>
2023-12-07 15:28:13 +0530

    uitill code

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,23 @@
 
 import dotenv from "dotenv"
-// import express from 'express';
-import connectDB from './db/index.js';
+import express from 'express';
+import connectDB from './db/db.js';
+
+const app=express();
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log("server is running on port " + process.env.PORT)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed: " + err)
+})
 
 // const app = express();
 
---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+const asyncHandler = (requestHandeler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandeler(req, res, next))
+        .catch((err) => next(err));
+    }
+}
+
+
+
+export { asyncHandler }
+
+
+
+
+// const asyncHandler = (fn) => (req, res, next) => { }
+// try {
+//     await fn(req, res, next);
+// } catch (error) {
+//     res.status(err.code || 500).json({
+//         success: false,
+//         message: err.message
+//     })
+// }
+
+
+// const asyncHandler = () => { }
+// const asyncHandler = (func) => () => { }
+// const asyncHandler = (func) => async () => { }
