70e9a9c61716884de3d9de63cf89f5a4830770d4
pradeepgr744 <dseeeap2020@gmail.com>
2023-12-07 23:10:40 +0530

    working token

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -56,7 +56,34 @@ userSchema.pre("save",async function (next){
 })
 
 userSchema.methods.isPasswordCorrect=async function(password){
+  return await bcrypt.compare(password, this.password)
+}
 
+userSchema.methods.generateAccessToken=async function(){
+    jwt.sign({
+
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+}
+
+userSchema.methods.generateRefreshToken=async function(){
+    jwt.sign({
+
+        _id:this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
 }
 
 export const user = mongoose.model('user', userSchema);
