22f282d3360007196fa4232efc035dbfed9d1919
pradeepgr744 <dseeeap2020@gmail.com>
2023-12-12 17:18:23 +0530

    update

--- server/src/controllers/user.controllers.js
+++ server/src/controllers/user.controllers.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
     //return res
 
     const { fullName, email, username, password, } = req.body
-    console.log("email:", email);
+    // console.log("email:", email);
 
     if (
         [fullName, email, username, password].some((field) =>
@@ -25,14 +25,19 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "All fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
@@ -56,11 +61,11 @@ const registerUser = asyncHandler(async (req, res) => {
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
-    if(!createdUser){
+    if (!createdUser) {
         throw new ApiError(500, "Something went wrong while creating the user")
     }
     return res.status(201).json(
-        new ApiResponse(200,createdUser,"User registerd Successfully")
+        new ApiResponse(200, createdUser, "User registerd Successfully")
     )
 })
 
--- server/src/index.js
+++ server/src/index.js
@@ -1,20 +1,16 @@
 
 import dotenv from "dotenv"
 import connectDB from './db/db.js';
-<<<<<<< HEAD
-import {app} from './app.js'
-=======
 import {app} from "./app.js"
->>>>>>> 53c0bc8b1312195a55b26e246d33659a1225b285
 
 dotenv.config({
-    path:'./env'
+    path:'./.env'
 })
 
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT||8000,()=>{
-        console.log("server is running on port " + process.env.PORT)
+        console.log(`server is running on port : ${process.env.PORT}`)
     })
 })
 .catch((err)=>{
