991cf94fbe6e63261b1484b175b25d3788471713
Pradeep <76429726+pradeepgr744@users.noreply.github.com>
2026-03-10 12:28:15 +0530

    Fix user lookup method in user.controller.js

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -133,7 +133,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
 
-    const loggedInUser = await User.findOne(user._id).select(
+    const loggedInUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
 
