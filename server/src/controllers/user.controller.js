939d0c143f456b6252ca504c7add49439baf676e
Pradeep <76429726+pradeepgr744@users.noreply.github.com>
2026-03-10 11:01:56 +0530

    Await password check for user authentication

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -232,7 +232,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
     const user = await User.findById(req.user?._id)
 
-    const isPasswordCorrect = user.isPasswordCorrect(oldPassword)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
     if (!isPasswordCorrect) {
         throw new ApiError(400, "Invalid old password")
