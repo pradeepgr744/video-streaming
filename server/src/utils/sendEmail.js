f5d731b680629023df909399958eb4d3878aff7a
Pradeep <76429726+pradeepgr744@users.noreply.github.com>
2026-07-30 13:11:16 +0530

    Add files via upload

---
+++ server/src/utils/sendEmail.js
@@ -0,0 +1,21 @@
+import nodemailer from "nodemailer"
+
+const transporter = nodemailer.createTransport({
+  service: "gmail", // Or configure your SMTP provider
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD, // App Password for Gmail
+  },
+});
+
+const sendEmail = async ({ to, subject, html, text }) => {
+  await transporter.sendMail({
+    from: `""<${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    text,
+    html,
+  });
+};
+
+export default sendEmail;
