267bf5cc53832657ddfc3c5fb67d274fe56a981e
Pradeep <76429726+pradeepgr744@users.noreply.github.com>
2026-07-30 13:56:07 +0530

    Add family property to email transporter options

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD, // App Password for Gmail
   },
+  family:4,
 });
 
 const sendEmail = async ({ to, subject, html, text }) => {
